efe315fae6283150a1cf2cccf8e84e2071a6d409
makram89 <marasss.kamin@gmail.com>
2021-06-10 22:08:37 +0200

    zadanie 8

---
+++ Zadanie_8/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+
+
+{
+    public class NewsItem
+    {
+
+        public int Id { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
